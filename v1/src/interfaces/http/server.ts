4b043e8168669fb070909a7edd6d709d314ed1ed
amr89-dev <amr89.dev@gmail.com>
2025-07-02 17:15:29 -0500

    feat: add cookie-parser middleware for handling HTTP cookies

--- v1/src/interfaces/http/server.ts
+++ v1/src/interfaces/http/server.ts
@@ -3,11 +3,13 @@ import cors from 'cors';
 import mainRoutes from './routes';
 import { initializePassport } from '../../infrastructure/auth/passport';
 import passport from 'passport';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 initializePassport();
 app.use(passport.initialize());
